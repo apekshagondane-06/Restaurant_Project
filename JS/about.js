f635aaa5327df10c28da4eec16d87fc89c57f827
Tanushree93 <52godsetanushree@gmail.com>
2025-07-15 23:12:27 +0530

    added

--- JS/about.js
+++ JS/about.js
@@ -1,12 +1,9 @@
- let currentFace = 0;
-    let angle = 0;
-    const cube = document.getElementById('cube');
+let currentFace = 0;
+let angle = 0;
+const cube = document.getElementById('cube');
 
-    function rotateCube() {
-        angle += 90;
-    //   currentFace = (currentFace + 1) % 4;
-    //    angle=currentFace + currentFace * 90;
-      cube.style.transform = `rotateY(-${angle}deg)`;
-    }
-
-    setInterval(rotateCube, 3000); // Rotate every 3 seconds
+function rotateCube() {
+  angle += 90;
+  cube.style.transform = `rotateY(-${angle}deg)`;
+}
+setInterval(rotateCube, 3000); 
