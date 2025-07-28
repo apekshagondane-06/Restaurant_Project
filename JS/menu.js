57ac211290b8659e34f68759305409a68f3fe9cc
apekshagondane-06 <apekshagondane945@gmail.com>
2025-07-28 20:13:44 +0530

    updated

---
+++ JS/menu.js
@@ -0,0 +1,38 @@
+//  order now button js //
+
+function openOrderModal(title, description, price, imageSrc) {
+  document.getElementById('modalTitle').innerText = title;
+  document.getElementById('modalDescription').innerText = description;
+  document.getElementById('modalPrice').innerText = price;
+  document.getElementById('modalImage').src = imageSrc;
+}
+
+function confirmOrder() {
+  const modal = bootstrap.Modal.getInstance(document.getElementById('orderModal'));
+  modal.hide();
+  
+  // Popup Alert
+  setTimeout(() => {
+    const alertDiv = document.createElement('div');
+    alertDiv.className = "alert alert-success text-center position-fixed top-0 start-50 translate-middle-x mt-3 px-4 shadow";
+    alertDiv.style.zIndex = 2000;
+    alertDiv.innerText = "✅ Order Confirmed Successfully!";
+    document.body.appendChild(alertDiv);
+
+    setTimeout(() => {
+      alertDiv.remove();
+    }, 2500);
+  }, 500);
+}
+//  Booking Modal alert js //
+ document.getElementById("reservationForm").addEventListener("submit", function (e) {
+    e.preventDefault(); 
+    document.getElementById("successAlert").classList.remove("d-none");
+    this.reset();
+    
+    setTimeout(() => {
+      document.getElementById("successAlert").classList.add("d-none");
+      const modal = bootstrap.Modal.getInstance(document.getElementById('bookingModal'));
+      modal.hide();
+    }, 2000);
+  });
