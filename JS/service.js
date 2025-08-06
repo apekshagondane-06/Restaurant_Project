e1761da4dae473a7e2142a4ef1fccdb81ed12d9a
Tanushree93 <52godsetanushree@gmail.com>
2025-08-06 20:39:01 +0530

    done

---
+++ JS/service.js
@@ -0,0 +1,12 @@
+//  Book a table sucessfull alert Js
+document.getElementById("reservationForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    document.getElementById("successAlert").classList.remove("d-none");
+    this.reset();
+
+    setTimeout(() => {
+        document.getElementById("successAlert").classList.add("d-none");
+        const modal = bootstrap.Modal.getInstance(document.getElementById('bookingModal'));
+        modal.hide();
+    }, 2000);
+});
